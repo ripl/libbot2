5b797940c79c064a4f341aedf5703c52202d7241
Francois Budin <francois.budin@kitware.com>
2018-05-17 18:01:42 -0400

    Enable creating packages on Ubuntu bionic,xenial, and trusty
    Package creation was broken because dpkg-shlibdeps could not find
    liblcm.so inside the created package. This is because the package
    is installing the tools and the libraries in a non-standard location.
    As a work around, we set R(UN)PATH to be able to find liblcm.so
    at packaging time. RPATH or RUNPATH are set depending on the
    default configuration of the compiler/linker on the current platform.
    CMake 3.6 is required to use $ORIGIN in R(UN)PATH for CPackDeb.cmake
    module to work.

--- scripts/package/linux/ubuntu/common/build.sh
+++ scripts/package/linux/ubuntu/common/build.sh
@@ -20,6 +20,7 @@ pushd /tmp
 git clone https://github.com/lcm-proj/lcm
 mkdir lcm-build
 pushd lcm-build
+
 # Forces the package to be installed in /opt/lcm/<version>
 # to be consistent with the fact these packages are not "official".
 cmake -DBUILD_SHARED_LIBS:BOOL=ON \
@@ -30,12 +31,14 @@ cmake -DBUILD_SHARED_LIBS:BOOL=ON \
       -DCPACK_DEBIAN_PACKAGE_VERSION="1.3.95.${timestamp}" \
       -DCPACK_DEBIAN_PACKAGE_MAINTAINER="Francois Budin <francois.budin@kitware.com>" \
       -DCPACK_PACKAGING_INSTALL_PREFIX="/opt/lcm/1.3.95.${timestamp}" \
+      -DCMAKE_C_FLAGS=-Wl,-rpath,\$ORIGIN/../lib \
+      -DCMAKE_CXX_FLAGS=-Wl,-rpath,\$ORIGIN/../lib \
       -DCMAKE_INSTALL_PREFIX="/opt/lcm/1.3.95.${timestamp}" \
       ../lcm
 make
 cpack -G DEB
 popd
-mv lcm-build/packages/lcm_1.3.95-1_amd64.deb lcm_1.3.95.${timestamp}-1_amd64.deb
+mv lcm-build/packages/lcm_1.3.95.${timestamp}-1_amd64.deb lcm_1.3.95.${timestamp}-1_amd64.deb
 rm -rf lcm-build
 
 # Install the package instead of running `make install`. This
@@ -45,6 +48,19 @@ rm -rf lcm-build
 # is hardcoded at compile time.
 dpkg -i lcm_1.3.95.${timestamp}-1_amd64.deb
 
+# Check if package versions are specified in environment variable
+JRE_VERSION=""
+if [ `echo "$CPACK_OPEN_JRE_VERSION"` != "" ]; then
+  JRE_VERSION="-DCPACK_OPEN_JRE_VERSION:STRING=${CPACK_OPEN_JRE_VERSION}"
+fi
+LIBPNG_VERSION=""
+if [ `echo "$CPACK_LIBPNG_VERSION"` != "" ]; then
+  LIBPNG_VERSION="-DCPACK_LIBPNG_VERSION:STRING=${CPACK_LIBPNG_VERSION}"
+fi
+CMAKE_PACKAGE_OPENGL=""
+if [ `echo "$PACKAGE_OPENGL"` == TRUE ]; then
+  CMAKE_PACKAGE_OPENGL="-DPACKAGE_OPENGL:BOOL=TRUE"
+fi
 # Configure, compile, and package libbot2
 mkdir libbot2-build
 pushd libbot2-build
@@ -54,6 +70,9 @@ cmake -DPACKAGE_LIBBOT2:BOOL=ON \
       -DCMAKE_C_FLAGS:STRING="$(dpkg-buildflags --get CFLAGS) $(dpkg-buildflags --get CPPFLAGS)" \
       -DCMAKE_PREFIX_PATH="/opt/lcm/1.3.95.${timestamp}" \
       -DCMAKE_SHARED_LINKER_FLAGS:STRING="$(dpkg-buildflags --get LDFLAGS)" \
+      $JRE_VERSION \
+      $LIBPNG_VERSION \
+      $CMAKE_PACKAGE_OPENGL \
       ../libbot2
 make
 cpack -G DEB
